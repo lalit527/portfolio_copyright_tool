5394d7498d21d350993525e863b2d935471aa732
Lalit-Yadav02 <lalit.yadav@freshworks.com>
2018-03-08 20:54:52 +0530

    Initial Commit

---
+++ lib/portfolio_copyright_tool.rb
@@ -0,0 +1,6 @@
+require "portfolio_copyright_tool/version"
+require "portfolio_copyright_tool/renderer"
+
+module PortfolioCopyrightTool
+  # Your code goes here...
+end
---
+++ lib/portfolio_copyright_tool/version.rb
@@ -0,0 +1,3 @@
+module PortfolioCopyrightTool
+  VERSION = "0.1.0"
+end
